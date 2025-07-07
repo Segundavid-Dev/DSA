e72db61e060c079e8497c035cc6840388c340a2e
Segundavid-Dev <segdavid03@gmail.com>
2025-07-07 14:08:17 +0100

    Array Data structures in python

--- List/main.py
+++ List/main.py
@@ -1,55 +1,55 @@
-# myList = ['a', 'apple', 1, 3.14]
+myList = ['a', 'apple', 1, 3.14]
 
 
-# # Initializing a list
-# example_list = [3.14159, "apple", 23]
-# empty_list = []
-# sequence_list = list(range(10))
+# Initializing a list
+example_list = [3.14159, "apple", 23]
+empty_list = []
+sequence_list = list(range(10))
 
-# print(example_list)
-# print(empty_list)
-# print(sequence_list)
+print(example_list)
+print(empty_list)
+print(sequence_list)
 
-# a_list = [2, "Educative", "A"]
+a_list = [2, "Educative", "A"]
 
-# def foo():
-#     print("Hello from foo()!")
-
-
-# another_list = [a_list, "Python", foo, ["Yet another list"]]
-
-# print(another_list[0]) #Elements of aList
-# print(another_list[0][1]) #Second element of aList
-# print(another_list[1]) #Python
-
-# # Append function
-# # Use this function to add a single element to the end of a list. This function works in O(1), constant time
-
-# list = [1, 3, 5, 'seven']
-# print(list)
-# list.append(9)
-# print(list)
+def foo():
+    print("Hello from foo()!")
 
 
-# # Insert function
-# # inserts an element to the list. use it like list.insert(index, value). It works in O(n) time. Try it out yourself
-# # Depends on the the input size of n
+another_list = [a_list, "Python", foo, ["Yet another list"]]
 
-# list.insert(0, 2)
-# print(list)
+print(another_list[0]) #Elements of aList
+print(another_list[0][1]) #Second element of aList
+print(another_list[1]) #Python
 
-# # Remove function 
-# # removes the given element at a given index.  use it like list.remove(element)
+# Append function
+# Use this function to add a single element to the end of a list. This function works in O(1), constant time
 
-# print(list)
-# list.remove("seven")
-# print(list)
-# # Runs in O(n)
-# # if value not in list, you get a run time value error
-# # list.remove(0)
+list = [1, 3, 5, 'seven']
+print(list)
+list.append(9)
+print(list)
 
-# list.reverse()
-# print(list)
+
+# Insert function
+# inserts an element to the list. use it like list.insert(index, value). It works in O(n) time. Try it out yourself
+# Depends on the the input size of n
+
+list.insert(0, 2)
+print(list)
+
+# Remove function 
+# removes the given element at a given index.  use it like list.remove(element)
+
+print(list)
+list.remove("seven")
+print(list)
+# Runs in O(n)
+# if value not in list, you get a run time value error
+# list.remove(0)
+
+list.reverse()
+print(list)
 
 # Why the .remove() is O(n) ?
 # The .remove(x) method searches from the beginning of the list to the end to find the first occurence of the elemnent x, then removes it
